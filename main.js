94d7225f74f65bb27849ef5d1656961e84730cf1
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-06-27 14:23:26 +0100

    added page about

--- main.js
+++ main.js
@@ -2,6 +2,5 @@ const navToggle = document.querySelector('.nav-toggle');
 const listLinks = document.querySelector('.list-links');
 
 navToggle.addEventListener('click', () => {
-  console.log('first');
   listLinks.classList.toggle('show-links');
 });
